e87f6c84e7d873bc5703275325da912be7c5e718
Daniel <danieljay.vh@gmail.com>
2024-06-17 14:38:13 +1200

    remove obj and bin + lesson 10

--- API/Program.cs
+++ API/Program.cs
@@ -8,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(
+        opt => {
+        opt.UseSqlite(
+                builder.Configuration.GetConnectionString("DefaultConnection")
+                );
+});
+
 
 var app = builder.Build();
 
